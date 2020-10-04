019a7861b2a6dee6ded2c4e9e71b9fe38afa43dc
thepassle <pascalschilp@gmail.com>
2020-10-04 13:33:13 +0200

    feat: support @property jsdoc

---
+++ packages/custom-elements-json-core/fixtures/jsdoc_prop/package/my-element.js
@@ -0,0 +1,29 @@
+/**
+ * @prop {boolean} prop1 - some description
+ * @property {number} prop2 - some description
+ * @property {string} prop3 - some description
+ * @property {Array} prop4 - some description
+ * @property {Object} prop5 - some description
+ */
+export class MyElement extends HTMLElement {
+  prop1;
+  prop2;
+
+  static properties = {
+    prop4: {type: Array}
+  }
+
+  static get properties() {
+    return {
+      prop5: {type: Object}
+    }
+  }
+
+  constructor() {
+    super();
+    this.prop1 = 'foo';
+    this.prop5 = 'bar';
+  }
+
+  get prop3(){}
+}
--- packages/custom-elements-json-core/src/index.ts
+++ packages/custom-elements-json-core/src/index.ts
@@ -1,7 +1,7 @@
 import { create } from './create';
 
 (async () => {
-  const packagePath = `${process.cwd()}/fixtures/default_exports/package`;
+  const packagePath = `${process.cwd()}/fixtures/-test/package`;
   console.log(packagePath);
 
   await create(packagePath);
