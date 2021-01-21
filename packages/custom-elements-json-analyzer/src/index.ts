a01fa5e5e3dbacd915ebcfbfb6acca53aa20ba2e
thepassle <pascalschilp@gmail.com>
2021-01-21 09:56:12 +0100

    chore: wip cli interface

--- packages/custom-elements-json-analyzer/src/index.ts
+++ packages/custom-elements-json-analyzer/src/index.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
 import { create } from './create';
+import commandLineArgs from 'command-line-args';
+
+const optionDefinitions = [
+  { name: 'glob', type: String, multiple: true, defaultOption: true, defaultValue: [ '**/*.js', '**/*.ts', '!**/.*.js', '!**/.*.ts', '!**/*.config.js', '!**/*.config.ts' ] },
+  { name: 'exclude', type: String, multiple: true },
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 (async () => {
   const packagePath = `${process.cwd()}/fixtures/functions_methods/package`;
