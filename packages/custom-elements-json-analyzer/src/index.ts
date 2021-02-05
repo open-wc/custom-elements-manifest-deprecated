fcb9ef865140bf250c05a3ce79acaf2a0076e59a
thepassle <pascalschilp@gmail.com>
2021-02-05 16:13:14 +0100

    feat: add customElementsManifest property to package.json

--- packages/custom-elements-json-analyzer/src/index.ts
+++ packages/custom-elements-json-analyzer/src/index.ts
@@ -2,6 +2,7 @@
 
 import { create } from './create';
 import commandLineArgs from 'command-line-args';
+import fs from 'fs';
 
 const optionDefinitions = [
   { name: 'glob', type: String, multiple: true, defaultOption: true, defaultValue: [ '**/*.{js,ts}', '!**/.*.{js,ts}', '!**/*.test.{js,ts}', '!**/*.config.{js,ts}' ] },
@@ -13,4 +14,13 @@ const options = commandLineArgs(optionDefinitions);
 (async () => {
   console.log(options);
   await create(options);
+
+  try {
+    const packageJsonPath = `${process.cwd()}/package.json`;
+    const packageJson = require(packageJsonPath);
+    packageJson.customElementsManifest = 'custom-elements.json';
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  } catch {
+    console.log(`Could not add 'customElementsManifest' property to ${process.cwd()}/package.json. \nAdding this property helps tooling locate your Custom Elements Manifest. Please consider adding it yourself, or file an issue if you think this is a bug.\nhttps://www.github.com/open-wc/custom-elements-manifest`);
+  }
 })();
